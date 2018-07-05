26a5c7f635549dc09e5f943e92f4838c463023fc
boukhemis11 <boukhemis2011@gmail.com>
2018-07-05 09:59:11 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,7 @@
-           $( document ).ready(function() {
-     var c1Title= $('#r1c1Title').text();
- 
-                          
-     localStorage.setItem("t1",c1Title);//changing c1Title to any String content like "test" will work
-    var result = localStorage.getItem("t1");
-    
-    $("#title1").html(result);
-    alert(result);
+$( document ).ready(function() {
+   var c1Title= $('#r1c1Title').text();              
+   localStorage.setItem("t1",c1Title);//changing c1Title to any String content like "test" will work
+   var result = localStorage.getItem("t1");
+   $("#title1").html(result);
+   alert(result);
 });
